790b7063087ce86d336bf18f501c45460910be19
Libin Biji <libinbiji43@gmail.com>
2023-11-24 17:54:55 +0530

    url update v1

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,9 +4,9 @@ const userRegisterAPI = (body: object) => API.post('/user/signup', body)
 
 const userLoginAPI = (body: object) => API.post('/user/login', body)
 
-const userFetchOnRefreshAPI = () => API.get('user/auth/refresh')
+const userFetchOnRefreshAPI = () => API.get('/user/auth/refresh')
 
-const userBookAppointmentAPI = (body: object) => API.post('user/auth/book', body)
+const userBookAppointmentAPI = (body: object) => API.post('/user/auth/book', body)
 
 export {
     userRegisterAPI,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ if(import.meta.env.MODE === 'production'){
     BASE_URL = 'https://appease-server.onrender.com/api/v1'
 }
 else{
-    BASE_URL = 'https://appease-server.onrender.com/api/v1'
+    BASE_URL = 'http://localhost:7000/api/v1'
 }
 
 export  {BASE_URL}
